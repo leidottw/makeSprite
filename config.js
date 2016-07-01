62f6839c21e23e1bd17b2c30d5d4a9af559c5ae9
leidottw <leidottw@gmail.com>
2016-07-01 16:18:44 +0800

    modify config

--- config.js
+++ config.js
@@ -14,99 +14,99 @@ exports = module.exports = {
         name: '_ipad_landscape',
         width: 1024,
         height: 768,
-        centerWidth: 440,
-        centerHeight: 440,
+        centerWidth: 400,
+        centerHeight: 400,
         footerHeight: 54
     }, {
         name: '_ipad_protrait',
         width: 768,
         height: 1024,
-        centerWidth: 440,
-        centerHeight: 440,
+        centerWidth: 400,
+        centerHeight: 400,
         footerHeight: 54
     }, {
         name: '_ipad_landscape@2x',
         width: 2048,
         height: 1536,
-        centerWidth: 886,
-        centerHeight: 886,
+        centerWidth: 846,
+        centerHeight: 846,
         footerHeight: 112
     }, {
         name: '_ipad_protrait@2x',
         width: 1536,
         height: 2048,
-        centerWidth: 886,
-        centerHeight: 886,
+        centerWidth: 846,
+        centerHeight: 846,
         footerHeight: 112
     }, {
         name: '_ipad_landscape@3x',
         width: 3072,
         height: 2304,
-        centerWidth: 1340,
-        centerHeight: 1340,
+        centerWidth: 1300,
+        centerHeight: 1300,
         footerHeight: 190
     }, {
         name: '_ipad_protrait@3x',
         width: 2304,
         height: 3072,
-        centerWidth: 1340,
-        centerHeight: 1340,
+        centerWidth: 1300,
+        centerHeight: 1300,
         footerHeight: 190
     }, {
         name: '_ipad_landscape@ipadpro',
         width: 2732,
         height: 2048,
-        centerWidth: 1180,
-        centerHeight: 1180,
+        centerWidth: 1140,
+        centerHeight: 1140,
         footerHeight: 146
     }, {
         name: '_ipad_protrait@ipadpro',
         width: 2048,
         height: 2732,
-        centerWidth: 1180,
-        centerHeight: 1180,
+        centerWidth: 1140,
+        centerHeight: 1140,
         footerHeight: 146
     }, {
         name: '_iphone_landscape@4s',
         width: 960,
         height: 640,
-        centerWidth: 464,
-        centerHeight: 464,
+        centerWidth: 370,
+        centerHeight: 370,
         footerHeight: 50
     }, {
         name: '_iphone_protrait@4s',
         width: 640,
         height: 960,
-        centerWidth: 464,
-        centerHeight: 464,
+        centerWidth: 370,
+        centerHeight: 370,
         footerHeight: 50
     }, {
         name: '_iphone_landscape@5',
         width: 1136,
         height: 640,
-        centerWidth: 464,
-        centerHeight: 464,
+        centerWidth: 446,
+        centerHeight: 446,
         footerHeight: 50
     }, {
         name: '_iphone_protrait@5',
         width: 640,
         height: 1136,
-        centerWidth: 464,
-        centerHeight: 464,
+        centerWidth: 446,
+        centerHeight: 446,
         footerHeight: 50
     }, {
         name: '_iphone_landscape@6',
         width: 1334,
         height: 750,
-        centerWidth: 530,
-        centerHeight: 530,
+        centerWidth: 510,
+        centerHeight: 510,
         footerHeight: 66
     }, {
         name: '_iphone_protrait@6',
         width: 750,
         height: 1334,
-        centerWidth: 530,
-        centerHeight: 530,
+        centerWidth: 510,
+        centerHeight: 510,
         footerHeight: 66
     }, {
         name: '_iphone_landscape@6plus',
